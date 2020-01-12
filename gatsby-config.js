a6f33c166f39f54d8bbf19141efd5f2a4aa02ed5
Nathan Walters <nwalters512@gmail.com>
2020-01-11 18:37:29 -0800

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Nathan's Cookbook`,
+    description: `My favorite recipes.`,
+    // Twitter handle
+    author: `@waltersn512`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
