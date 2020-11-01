6ca0fc880fefabcf7181e2fbb67adbda18504561
Nathan Walters <nwalters512@gmail.com>
2020-11-01 15:57:27 -0800

    Drop unneeded files

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-// import "./layout-old.css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
