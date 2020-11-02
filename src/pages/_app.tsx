061786c48d1116adca14a8f818b5327195b24170
Nathan Walters <nwalters512@gmail.com>
2020-11-01 21:40:41 -0800

    Add back deleted file

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import type { AppProps } from "next/app"
+
+import Header from "../components/header"
+
+import "../tailwind.css"
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <React.Fragment>
+      <Header title="Nathan's Cookbook" />
+      <div className="w-full max-w-screen-md mx-auto mt-3">
+        <Component {...pageProps} />
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default App
