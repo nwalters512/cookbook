d48c76cb0404bccf0c5a3f0f918d0c9bf439dc49
Nathan Walters <nwalters512@gmail.com>
2020-11-01 17:29:45 -0800

    Add Google font

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import NextDocument, { Html, Head, Main, NextScript } from "next/document"
+
+class Document extends NextDocument {
+  static async getInitialProps(ctx) {
+    const initialProps = await NextDocument.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default Document
