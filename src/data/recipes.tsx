91cb649bacd4b8b351f4cba5375062b76bc45cd6
Nathan Walters <nwalters512@gmail.com>
2020-11-01 16:54:00 -0800

    Add functioning index page

---
+++ src/data/recipes.tsx
@@ -0,0 +1,58 @@
+import fs from "fs-extra"
+import path from "path"
+import yaml from "js-yaml"
+
+export interface Recipe {
+  slug: string
+  title: string
+  description?: string
+  ingredients: string[]
+  steps: string[]
+  notes?: string[]
+}
+
+export interface RecipesBySlug {
+  [slug: string]: Recipe
+}
+
+export interface RecipeData {
+  recipes: Recipe[]
+  recipesBySlug: RecipesBySlug
+}
+
+let cache: RecipeData = null
+
+export const getAllRecipes = async (): Promise<RecipeData> => {
+  if (cache) return cache
+  const recipes = await fs.readdir(path.join(process.cwd(), "recipes"))
+  const recipeData = await Promise.all(
+    recipes.map(async (slug) => {
+      const rawData = await fs.readFile(
+        path.join(process.cwd(), "recipes", slug, "index.yml"),
+        "utf-8"
+      )
+      const parsedData = yaml.safeLoad(rawData) as Recipe
+      return {
+        ...parsedData,
+        slug,
+      }
+    })
+  )
+  const recipesBySlug = recipeData.reduce(
+    (acc, data) => ({
+      ...acc,
+      [data.slug]: data,
+    }),
+    {} as RecipesBySlug
+  )
+  cache = {
+    recipes: recipeData,
+    recipesBySlug,
+  }
+  return cache
+}
+
+export const getRecipeBySlug = async (slug: string): Promise<Recipe> => {
+  const data = await getAllRecipes()
+  return data.recipesBySlug[slug]
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
 import React from "react"
+import { GetStaticProps } from "next"
+import Link from "next/link"
 
-const Index: React.FC = () => {
-  return <div>Hello, world!</div>
+import { getAllRecipes, Recipe } from "../data/recipes"
+
+interface IndexProps {
+  recipes: Recipe[]
+}
+
+const Index: React.FC<IndexProps> = ({ recipes }) => {
+  return (
+    <React.Fragment>
+      {recipes.map((recipe) => (
+        <article className="mb-4">
+          <Link href={`/recipe/${recipe.slug}`}>
+            <h2 className="text-2xl hover:text-pink-800 focus:text-pink-800">
+              {recipe.title}
+            </h2>
+          </Link>
+          {recipe.description && <p>{recipe.description}</p>}
+        </article>
+      ))}
+    </React.Fragment>
+  )
 }
 
 export default Index
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { recipes } = await getAllRecipes()
+  return { props: { recipes } }
+}
