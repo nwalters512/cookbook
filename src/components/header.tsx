4dc16257bdb49b28b501207faa5cfa6ca7759e01
Nathan Walters <nwalters512@gmail.com>
2020-11-01 17:02:55 -0800

    Work on layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,11 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import Link from "next/link"
 
-const Header = ({ siteTitle }) => (
+interface HeaderProps {
+  title: string
+}
+
+const Header = ({ title }) => (
   <header
     style={{
       background: `rebeccapurple`,
@@ -17,26 +20,19 @@ const Header = ({ siteTitle }) => (
       }}
     >
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+        <Link href="/">
+          <a
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {title}
+          </a>
         </Link>
       </h1>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import type { AppProps } from "next/app"
+
+import Header from "../components/header"
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <React.Fragment>
+      <Header title="Nathan's Cookbook" />
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
+}
+
+export default App
