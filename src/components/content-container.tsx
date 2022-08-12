12118a85e4dae9d2e1e728ea9ce67daea8d53ed8
Nathan Walters <nwalters512@gmail.com>
2022-08-12 15:41:17 -0700

    Fix children props

--- src/components/content-container.tsx
+++ src/components/content-container.tsx
@@ -1,6 +1,10 @@
 import React from "react"
 
-const ContentContainer: React.FC = ({ children }) => {
+interface ContentContainerProps {
+  children: React.ReactNode
+}
+
+const ContentContainer: React.FC<ContentContainerProps> = ({ children }) => {
   return <div className="w-full max-w-screen-md mx-auto px-3">{children}</div>
 }
 
