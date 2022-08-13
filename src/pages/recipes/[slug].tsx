ba41c0f68afaf087a97366e3220551f9d947232f
Nathan Walters <nwalters512@gmail.com>
2022-08-12 17:33:51 -0700

    Format ingredient items

--- src/pages/recipes/[slug].tsx
+++ src/pages/recipes/[slug].tsx
@@ -8,6 +8,7 @@ import SEO from "../../components/seo"
 import { useCheckboxes } from "../../hooks/useCheckboxes"
 import { Checkbox } from "../../components/checkbox"
 import { replaceFractionsWithUnicode } from "../../data/recipes"
+import classNames from "classnames"
 
 interface RecipePathsParams {
   slug: string
@@ -19,8 +20,108 @@ interface SectionTitleProps {
 }
 
 const SectionTitle: React.FC<SectionTitleProps> = ({ children }) => (
-  <h2 className="text-xl font-bold mt-4 mb-2">{children}</h2>
+  <h2 className="text-xl font-bold mt-4 mb-2 uppercase">{children}</h2>
 )
+
+const NUMBER = "\\d+"
+const FRACTION = `${NUMBER}\/${NUMBER}`
+const UNICODE_FRACTION = "[\\u00BC-\\u00BE\\u2150-\\u215F]"
+const MIXED_FRACTION = `(?:${NUMBER}\\s+(?:${FRACTION}|${UNICODE_FRACTION}))`
+const RANGE_PART = `(?:${NUMBER}|${FRACTION}|${UNICODE_FRACTION})`
+const RANGE = `(?:${RANGE_PART}\\s*(?:-|to)\\s*${RANGE_PART})`
+const QUANTITY = `(?:${RANGE}|${MIXED_FRACTION}|${RANGE_PART})`
+const UNITS = [
+  // Weight
+  "oz",
+  "ozs",
+  "ounce",
+  "ounces",
+  "lb",
+  "lbs",
+  "pound",
+  "pounds",
+  // Volume
+  "tsp",
+  "teaspoon",
+  "teaspoons",
+  "tbsp",
+  "tablespoon",
+  "tablespoons",
+  "cup",
+  "cups",
+  "pint",
+  "pints",
+  "quart",
+  "quarts",
+  // Containers
+  "jar",
+  "jars",
+  "can",
+  "cans",
+  "box",
+  "boxes",
+  "package",
+  "packages",
+  // Misc
+  "clove",
+  "cloves",
+  "slice",
+  "slices",
+  "pinch",
+  "pinches",
+  "strip",
+  "strips",
+  "dash",
+  "dashes",
+].join("|")
+const QUANTITY_AND_UNITS = new RegExp(
+  `(${QUANTITY}?\\s*(?:(?:${UNITS})\\s+)*)?(.*)`,
+  "i"
+)
+console.log(QUANTITY_AND_UNITS)
+
+function renderParenthesizedInfo(info: string): React.ReactNode {
+  const match = info.match(/\([^\)]+\)/g)
+  if (!match) return info
+
+  // Break string into portions that are not parenthesized and those that are.
+  let components: string[] = [info]
+  for (const part of match) {
+    components = components.flatMap((c) => {
+      let split2 = c.split(part)
+      return split2
+        .flatMap((s) => [s, part])
+        .slice(0, -1)
+        .filter((s) => s.length > 0)
+    })
+  }
+
+  return components.map((c, i) => (
+    <span
+      key={`${i}-${c}`}
+      className={classNames({
+        "text-slate-400": c.startsWith("("),
+      })}
+    >
+      {c}
+    </span>
+  ))
+}
+
+function renderIngredient(ingredient: string): React.ReactNode {
+  const match = ingredient.match(QUANTITY_AND_UNITS)
+  if (!match) return renderParenthesizedInfo(ingredient)
+
+  const maybeQuantity = match[1]
+  if (!maybeQuantity) return renderParenthesizedInfo(ingredient)
+
+  return (
+    <span>
+      <strong>{maybeQuantity}</strong> {renderParenthesizedInfo(match[2])}
+    </span>
+  )
+  return ingredient
+}
 
 interface RecipeProps {
   title: string
@@ -58,7 +159,10 @@ const Recipe: React.FC<RecipeProps> = ({
           const ingredientKey = `${i}-${ingredient}`
           const isChecked = !!checked[ingredientKey]
           return (
-            <li className="flex flex-row items-center mb-2" key={ingredient}>
+            <li
+              className="flex flex-row items-center mb-2"
+              key={ingredient + i.toString()}
+            >
               <Checkbox
                 className="mr-2"
                 checked={isChecked}
@@ -67,7 +171,7 @@ const Recipe: React.FC<RecipeProps> = ({
                 }}
               />
               <span className={isChecked ? "text-gray-400" : ""}>
-                {ingredient}
+                {renderIngredient(ingredient)}
               </span>
             </li>
           )
