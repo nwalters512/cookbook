efd24b6b9c9c3f0ec6cbefab7b6e5116da63257d
Nathan Walters <nwalters512@gmail.com>
2023-03-19 15:33:56 -0700

    Use built-in Next.js font functionality

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import type { AppProps } from "next/app"
-import { Source_Sans_Pro } from "@next/font/google"
+import { Source_Sans_Pro } from "next/font/google"
 
 import Header from "../components/header"
 
